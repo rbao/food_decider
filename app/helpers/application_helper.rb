b6f78c6b70ea408f0d4f10793fe2f96afeadf9f4
Roy Bao <roybao2010@gmail.com>
2013-06-09 20:20:09 -0400

    Setup some basic view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def icon_link(path, icon_class, html_options = {})
+    icon_content = content_tag(:i, nil, :class => icon_class)
+    content = link_to icon_content, path, html_options
+    content
+  end
+
+  def flash_css_class(flash_key)
+    case flash_key
+      when :notice then "alert alert-success"
+      when :info then "alert alert-info"
+      when :warning then "alert"
+      when :alert then "alert alert-error"
+    end
+  end
 end
