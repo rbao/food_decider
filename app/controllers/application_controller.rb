cc516c1e358953ef0c437a2d93e61b8a71608f74
Roy Y. Bao <roybao2010@gmail.com>
2013-06-14 12:38:08 -0400

    Add basic auth to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate
+
+  protected
+    def require_login?
+      false
+    end
+
+    def authenticate
+      return true unless require_login?
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == 'admin' && password == 'test1234'
+      end
+    end
 end
