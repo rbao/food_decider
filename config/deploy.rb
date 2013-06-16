083af12994ef340d3da9d41124f536c147da2598
Roy Bao <roybao2010@gmail.com>
2013-06-16 16:49:46 -0400

    Add deployment script

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'bundler/capistrano'
+
+# Multi-Stage
+set :stages, %w(production local_staging)
+set :default_stage, "local_staging"
+require 'capistrano/ext/multistage'
+
+# Load 3rd-party recipes
+require "rbao_cap_recipes/base"
+require "rbao_cap_recipes/nginx"
+require "rbao_cap_recipes/unicorn"
+require "rbao_cap_recipes/shared_config"
+require "rbao_cap_recipes/check"
+require "rbao_cap_recipes/db"
+require "rbao_cap_recipes/git"
+require "rbao_cap_recipes/secret_token"
+
+# Stage specific settings will override
+set :user, "www-data"
+set :application, "food_decider"
+set(:deploy_to) { "/var/www/#{application}" }
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :shared_config_files, [['database.yml.sample', 'database.yml'],
+                           ['application.yml.sample', 'application.yml']]
+set :scm, :git 
+set :repository,  "git://github.com/rbao/food_decider.git"
+set :branch, "master"
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
