e4c1a05f35dd8fe32aab787b4b2c0b9fb30f4169
Roy Bao <roybao2010@gmail.com>
2013-06-16 23:54:32 -0400

    Allow price blank

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,14 +3,14 @@ class Order < ActiveRecord::Base
 
   validates :name, presence: true
   validates :item, presence: true
-  validates :price, numericality: true
-  validates :tax, numericality: true, if: :no_error_on_price
-  validates :total, numericality: true, if: :no_error_on_price
+  validates :price, numericality: true, allow_blank: true
+  validates :tax, numericality: true, if: :no_error_on_price_and_price_is_not_blank
+  validates :total, numericality: true, if: :no_error_on_price_and_price_is_not_blank
 
   delegate :restaurant, :tax_rate, :to => :group_order
 
   private
-    def no_error_on_price
-      !errors.has_key?(:price)
+    def no_error_on_price_and_price_is_not_blank
+      !errors.has_key?(:price) && !price.blank?
     end
 end
